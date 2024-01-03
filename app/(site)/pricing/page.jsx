a0dba0181ebe3009f119b41c65136fd07c433791
maxbezs <baks.bezsmertnuy@gmail.com>
2024-01-03 04:18:12 +0100

    1

--- app/(site)/pricing/page.jsx
+++ app/(site)/pricing/page.jsx
@@ -242,6 +242,167 @@ const Pracing = () => {
           </div>
         </section>
       </div>
+      <div className="max-w-5xl mx-auto my-8 px-8 lg:px-0">
+        <H2 styles="text-center ">
+          So easy to use, simple as it is. How does it work?
+        </H2>
+        <div className="grid  grid-cols-1 lg:grid-cols-3 gap-3">
+          <div className="flex gap-2 items-center border-neutral-800 border  drop-shadow-[0_0px_1px_rgba(256,256,256,0.35)]  rounded-md p-4">
+            <svg
+              fill="none"
+              height="24"
+              shape-rendering="geometricPrecision"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              width="24"
+              className="w-auto h-12"
+            >
+              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+              <path d="M22 6l-10 7L2 6" />
+            </svg>
+            <p className="w-full lg:w-3/4">
+              Subscribe to a plan & request as many tasks as you'd like.
+            </p>
+          </div>
+          <div className="flex gap-2 items-center border-neutral-800 border  drop-shadow-[0_0px_1px_rgba(256,256,256,0.35)]  rounded-md p-4">
+            <svg
+              fill="none"
+              height="24"
+              shape-rendering="geometricPrecision"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              width="24"
+              className="w-auto h-12"
+            >
+              <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z" />
+            </svg>
+            <p className="w-full lg:w-3/4">
+              Receive ready to use soft/feature within a few days. No Weekends
+            </p>
+          </div>
+          <div className="flex gap-2 items-center border-neutral-800 border  drop-shadow-[0_0px_1px_rgba(256,256,256,0.35)]  rounded-md p-4">
+            <svg
+              className="w-auto h-12"
+              fill="none"
+              height="24"
+              shape-rendering="geometricPrecision"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              width="24"
+            >
+              <path d="M23 4v6h-6" />
+              <path d="M1 20v-6h6" />
+              <path d="M3.51 9a9 9 0 0114.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0020.49 15" />
+            </svg>
+            <p className="w-full lg:w-3/4">
+              We'll revise the results until you're 100% satisfied.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-3xl mx-auto my-8 px-8 lg:px-0">
+        <H2 styles="text-center ">We build anything for Your business:</H2>
+        <div>
+          <div className="flex gap-2 my-6">
+            <div className="bg-white w-fit text-black rounded-full px-3 py-1 ">
+              Popular
+            </div>
+            <div className="bg-neutral-950 border-neutral-800 border rounded-full px-3 py-1 ">
+              Hospitality
+            </div>
+            <div className="bg-neutral-950 border-neutral-800 border rounded-full px-3 py-1 ">
+              Finance
+            </div>
+          </div>
+          <div className="mx-auto max-w-4xl overflow-hidden border-neutral-800 border rounded-md grid grid-cols-3   ">
+            <div className="flex flex-col border-r-neutral-800  border-r last:border-none">
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Website</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Mobile app</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Logos &amp; branding</div>
+              </div>
+
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>SAAS</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Wireframes</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Stationery</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Pitch decks</div>
+              </div>
+            </div>
+            <div className="flex flex-col border-r-neutral-800  border-r last:border-none">
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Slide decks</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Direct mail</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Infographics</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Brochures</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Signage</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Brand guides</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Social media graphics</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Billboards</div>
+              </div>
+            </div>
+            <div className="flex flex-col border-r-neutral-800  border-r last:border-none">
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Trade show banners</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Email graphics</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Resumes</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Business cards</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Packaging</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Blog graphics</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Digital ads</div>
+              </div>
+              <div className=" p-4 bg-neutral-950 border-b border-b-neutral-800 last:border-none">
+                <div>Icons</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className=" max-w-xl mx-auto px-8 my-8">
         <H2 styles="mx-auto w-fit">FAQs</H2>
         <div id="faq">
